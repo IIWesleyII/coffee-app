d7c13ec6369faf8f90e57d2925b38449482db8fc
leaf-turner <wesley.shaw.475@my.csun.edu>
2022-01-28 13:19:34 -0800

    added menu prompt to interact with database

---
+++ app.py
@@ -0,0 +1,49 @@
+import db
+
+MENU_PROMPT = '''
+-- COFFEE BEAN APP --
+
+Please choose an option:
+
+1. Add a new bean
+2. See all beans
+3. Find a bean by name
+4. See which preparation method is best for a bean
+5. Exit
+
+Your selection:
+'''
+
+
+
+def menu():
+    conn = db.connect()
+    db.create_table(conn)
+
+    while (user_input := input(MENU_PROMPT)) != '5':
+        if user_input == '1':
+            name = input('Enter coffee bean name: ')
+            method = input('Enter method in preparing coffee bean: ')
+            rating = int(input('Enter bean rating: '))
+            db.add_bean(conn,name,method,rating)
+
+        elif user_input == '2':
+            beans = db.get_all_beans(conn)
+            for bean in beans:
+                print(bean)
+
+        elif user_input == '3':
+            name = input('Enter coffee bean name: ')
+            beans = db.get_beans_by_name(conn,name)
+            for bean in beans:
+                print(bean)
+            
+        elif user_input == '4':
+            name = input("Enter bean name to find: ")
+            best_method = db.get_best_preparation_by_bean(conn,name)
+            print(f'The best preparation method for {name} is {best_method[2]}.')
+
+        else:
+            print('\n\n! INVALID INPUT !\n\n')
+
+menu()
--- db.py
+++ db.py
@@ -2,24 +2,69 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 load_dotenv()
-try:
-    conn = psycopg2.connect(
-        user ='postgres',
-        password= os.getenv('DB_PASSWORD'),
-        host='localhost',
-        database='coffee-app'
-    )
+
+
+CREATE_BEANS_TABLE = 'CREATE TABLE IF NOT EXISTS beans (id SERIAL PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);'
+INSERT_BEAN = 'INSERT INTO beans (name,method,rating) VALUES (%s,%s,%s);'
+GET_ALL_BEANS = 'SELECT * FROM beans'
+GET_BEANS_BY_NAME = 'SELECT * FROM beans WHERE name = %s;'
+GET_BEST_PREPARATION_FOR_BEAN = '''
+SELECT * FROM beans
+WHERE name = %s
+ORDER BY rating DESC
+LIMIT 1;
+'''
+
+def connect():
+    try:
+        conn = psycopg2.connect(
+            user ='postgres',
+            password= os.getenv('DB_PASSWORD'),
+            host='localhost',
+            database='coffee-app'
+        )
+    except ConnectionError as exc:
+        raise RuntimeError('Failed to open database') from exc
+
+    return conn
     
-    
+
+def create_table(conn): 
     with conn:
         cursor = conn.cursor()
-        cursor.execute(
-            'CREATE TABLE beans (id SERIAL PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);'
-        )
-
-        print('yipeeeee')
-
+        cursor.execute(CREATE_BEANS_TABLE)
         cursor.close()
+
+
+def add_bean(conn,name,method,rating):
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(INSERT_BEAN, (name,method,rating))
+        cursor.close()
+
+
+def get_all_beans(conn):
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(GET_ALL_BEANS)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
         
-except ConnectionError as exc:
-    raise RuntimeError('Failed to open database') from exc
+
+def get_beans_by_name(conn, name):
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(GET_BEANS_BY_NAME, (name,))
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+       
+
+def get_best_preparation_by_bean(conn,name):
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(GET_BEST_PREPARATION_FOR_BEAN, (name,))
+        result = cursor.fetchall()
+        cursor.close()
+        return result
